04423bfd1c8387fa923e80dd9ccde6c39a9cfd37
aashirwadwardhan <96619168+aashirwadwardhan@users.noreply.github.com>
2023-04-23 18:10:01 +0530

    Update app.py

--- spam_SMS_detector/app.py
+++ spam_SMS_detector/app.py
@@ -36,8 +36,8 @@ def transform_text(text):
 # tfidf = pickle.load(open('vectorizer.pkl','rb'))
 # model = pickle.load(open('model.pkl','rb'))
 
-tfidf = pickle.load(open('./vectorizer.sav', 'rb'))
-model = pickle.load(open('./model.sav', 'rb'))
+tfidf = pickle.load(open('spam_SMS_detector/vectorizer.sav', 'rb'))
+model = pickle.load(open('spam_SMS_detector/model.sav', 'rb'))
 st.title("Email/SMS_Spam_Classifier")
 
 input_sms = st.text_area("Enter the message")
